457b81137446cc0662cd1ee5b2b5092170c3779e
ag89201 <ag89201@gmail.com>
2018-12-25 17:43:16 +0300

    Simple Editor on Qt
    first commit

---
+++ editor.h
@@ -0,0 +1,42 @@
+#ifndef MDICHILD_H
+#define MDICHILD_H
+
+#include <QTextEdit>
+
+class Editor : public QTextEdit
+{
+    Q_OBJECT
+
+public:
+    Editor(QWidget *parent = 0);
+
+    void newFile();
+    bool save();
+    bool saveAs();
+    QSize sizeHint() const;
+    QAction *windowMenuAction() const { return action; }
+
+    static Editor *open(QWidget *parent = nullptr);
+    static Editor *openFile(const QString &fileName,
+                            QWidget *parent = nullptr);
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void documentWasModified();
+
+private:
+    bool okToContinue();
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    bool readFile(const QString &fileName);
+    bool writeFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
+
+    QString curFile;
+    bool isUntitled;
+    QAction *action;
+};
+
+#endif
